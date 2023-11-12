5903517186d2862d2b6559ac1c4cf77c254da60c
Shah Mir <chand14ok@gmail.com>
2023-11-12 12:45:37 +0500

    toast text color update

--- components/ui/toast.jsx
+++ components/ui/toast.jsx
@@ -23,7 +23,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-blue-500 text-muted-foreground",
+        default: "border bg-blue-500 text-white",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
